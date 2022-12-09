dff84206a83fa9016c191385937df6d1807146f7
CollinKite <me@collinkite.com>
2022-12-08 17:36:20 -0700

    Initial Commit

---
+++ src/main/java/kite/collin/fantasyui/FantasyUiApplication.java
@@ -0,0 +1,13 @@
+package kite.collin.fantasyui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasyUiApplication.class, args);
+	}
+
+}
